a5694190dc16ca18b8c4068d83c99da4b24c340a
jhc001 <964706001@qq.com>
2016-07-22 18:14:14 +0800

    what commit ^^

--- model.js
+++ model.js
@@ -2,37 +2,72 @@
     window.Point = function Point(x,y){
         this.x =x;
         this.y =y;
-    }
-
+    };
+    /*获取元素坐标*/
     Element.prototype.getPoint = function (){
         return new window.Point(this.offsetLeft,this.offsetTop);
-        
-    }
+    };
+    /*计算两元素的距离*/
+    Element.prototype.dist = function (e){
+        if(!e) throw 'the e is not be null';
+        var p1 = this.getPoint();
+        var p2 = e.getPoint();
+        return new Point(p1.x-p2.x,p1.y-p2.y);
+    };
 
     function God(env){
         if(this.constructor===God){
-            if(env===null||env.nodeName!='DIV')throw "the  environment must be a  div";
+            if(env===null||env===undefined)throw "the  environment can't be null";
             this.env = env;
             this.init();
         }else{
-            return new God();
+            return new God(env);
         }
-    }
+    };
 
     God.prototype = {
         constructor:God,
-        init:function(){
-            var divs = this.env.querySelectorAll("div");
-            this.length=0;
-            for(i in divs){
-                this[i]=divs[i];
-                this.length++;
-            }
+        d:10
+    };
+
+    God.prototype.initElement = function(obj){
+        var p = this.randomPoint();
+        
+        $(obj).css({position:"absolute",
+                    display:"inline-block",
+                    width:this.d+"px",
+                    height:this.d+"px",
+                    background:"red"});
+        obj.style.left = p.x+"px";
+        obj.style.top = p.y+"px";
+    };
+
+   God.prototype.randomPoint = function(){
+        var x = Math.random()*(this.env.offsetWidth-this.d);
+        var y = Math.random()*(this.env.offsetHeight-this.d);
+       
+        var rx = parseInt(x)+this.env.offsetLeft;
+        var ry = parseInt(y)+this.env.offsetTop;
+        if(rx>300 || ry>300){
+            console.log("fasdfsd");
         }
-    }
+        return new Point(rx,ry);
+   }
 
+    God.prototype.init = function(){
+        var divs = this.env.querySelectorAll("div");
+        this.length=0;
+        if(divs&&divs.length>0)
+        for(var i=0;i<divs.length;i++){
+            var e = divs[i];
+            this[i]=e;
+            this.length++;
+            var info ={};
+            e.info = info;
+            this.initElement(e);
+        }
+     };
 
-
-    
+    window.God = God;
 
 }())
--- query.js
+++ query.js
@@ -2,8 +2,8 @@
     function Jhc(sele){
         if(this.constructor===Jhc){
             this.target =[];
-            this.select(sele);
             this.length=0;
+            this.select(sele);
         }else{
             return new Jhc(sele);
         }
@@ -12,13 +12,116 @@
     Jhc.prototype={
         constructor:Jhc,
         select:function(stor){
-            var t = document.querySelectorAll(stor);
-            for(i in t) {
-                this[i] = t[i];
-                this.length++;
+            if(!stor) return ;
+            if(typeof stor==='string' && stor[0]==='#'){
+                this[0] = document.getElementById(stor.substr(1));
+                this.length=1;
+            }else if(typeof stor==='string'){
+                var t = document.querySelectorAll(stor);
+                for(i=0;i<t.length;i++) {
+                    this[i] = t[i];
+                    this.length++;
+                }
+            }else if(stor.nodeType===1){
+                this[0] = stor;
+                this.length=1;
             }
-            
         }
+    }
+
+    Jhc.prototype.attr = function(aname,val){
+        if(!aname) throw "the attrbute name is not be null";
+        if(this.length<1)return ;
+        if(val===undefined){
+            return this[0].getAttribute(aname);
+        }else{
+            var s = "";
+           for(var i=0;i<this.length;i++){
+               this[i].setAttribute(aname,val);
+               var val = this[i].getAttribute(aname);
+               if(val){
+                s+=val;
+               }
+           }
+           return s;
+        }
+    }
+
+    Jhc.prototype.first = function(){
+        return new Jhc(this[0]);
+    }
+
+    Jhc.prototype.text = function(tx){
+        var resu;
+        if(this.length>0){
+            resu = this[0].innerText;
+        }else{
+            return null;
+        }
+        if(!(tx===undefined)){
+            for(var i=0;i<this.length;i++){
+                this[i].innerText = tx;
+            }
+        }
+        return resu;
+        
+    }
+
+    Jhc.prototype.html = function(ht){
+        var resu;
+        if(this.length>0){
+            resu = this[0].innerHTML;
+        }else{
+            return null;
+        }
+        if(!(ht===undefined)){
+            for(var i=0;i<this.length;i++){
+                this[i].innerHTML = ht;
+            }
+        }
+        return resu;
+        
+    }
+
+    Jhc.prototype.val = function(v){
+        var old;
+        if(this.length>0){
+            old = this[0].value;
+        }else{
+            return null;
+        }
+        if(!(v===undefined)){
+            for(var i=0;i<this.length;i++){
+                this[i].value = v;
+            }
+        }
+        return old;
+    }
+/*
+    background:red
+    background
+    {background:red,color:red}
+*/
+    Jhc.prototype.css = function(aname,val){
+        if(this.length<1)return null;
+        var resu;
+        if(typeof aname==='object'){
+            for(var i=0;i<this.length;i++){
+                for(k in aname){
+                    this[i].style[k] = aname[k];
+                }
+            }
+        }else if(typeof aname==='string'){
+            if(!(val===undefined)){
+                for(var i=0;i<this.length;i++){
+                    this[i].style[aname]=val;
+                }
+            }
+            resu = this[0].style[aname];
+        }else{
+            throw "the function css arguments type must be a string or object";
+        }
+        return resu;
     }
     window.$ = window.Jhc = Jhc;
 }())
